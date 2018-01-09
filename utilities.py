68f4eac8d07fa121ab9a7b1477c46071d9f5fa65
Ziv Yaniv <zivyaniv@nih.gov>
2018-01-09 10:11:14 -0500

    Adding advanced_registration notebook.
    Adding notebook illustrating the use of masks, FFD, Demons and various
    registration evaluation approaches.

--- utilities.py
+++ utilities.py
@@ -1,6 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+popi_body_label = 0
+popi_air_label = 1
+popi_lung_label = 2
+
+def read_POPI_points(file_name):
+    """
+    Read the Point-validated Pixel-based Breathing Thorax Model (POPI) landmark points file.
+    The file is an ASCII file with X Y Z coordinates in each line and the first line is a header.
+
+    Args:
+       file_name: full path to the file.
+    Returns:
+       (list(tuple)): List of points as tuples.
+    """
+    with open(file_name,'r') as fp:
+        lines = fp.readlines()
+        points = []
+        # First line in the file is #X Y Z which we ignore.
+        for line in lines[1:]:
+            coordinates = line.split()
+            if coordinates:
+                points.append((float(coordinates[0]), float(coordinates[1]), float(coordinates[2])))
+        return points
+
+
 def point2str(point, precision=1):
     """
     Format a point for printing, based on specified precision with trailing zeros. Uniform printing for vector-like data 
@@ -33,15 +58,63 @@ def uniform_random_points(bounds, num_points):
     return list(mat[:len(bounds)].T)
 
 
-def target_registration_errors(tx, point_list, reference_point_list):
-    """
-    Distances between points transformed by the given transformation and their
-    location in another coordinate system. When the points are only used to evaluate
-    registration accuracy (not used in the registration) this is the target registration
-    error (TRE).
-    """
-    return [np.linalg.norm(np.array(tx.TransformPoint(p)) -  np.array(p_ref))
-          for p,p_ref in zip(point_list, reference_point_list)]
+def target_registration_errors(tx, point_list, reference_point_list,
+                               display_errors = False, min_err= None, max_err=None, figure_size=(8,6)):
+  """
+  Distances between points transformed by the given transformation and their
+  location in another coordinate system. When the points are only used to
+  evaluate registration accuracy (not used in the registration) this is the
+  Target Registration Error (TRE).
+
+  Args:
+      tx (SimpleITK.Transform): The transform we want to evaluate.
+      point_list (list(tuple-like)): Points in fixed image
+                                     cooredinate system.
+      reference_point_list (list(tuple-like)): Points in moving image
+                                               cooredinate system.
+      display_errors (boolean): Display a 3D figure with the points from
+                                point_list color corresponding to the error.
+      min_err, max_err (float): color range is linearly stretched between min_err
+                                and max_err. If these values are not given then
+                                the range of errors computed from the data is used.
+      figure_size (tuple): Figure size in inches.
+
+  Returns:
+   (errors) [float]: list of TRE values.
+  """
+  transformed_point_list = [tx.TransformPoint(p) for p in point_list]
+
+  errors = [np.linalg.norm(np.array(p_fixed) -  np.array(p_moving))
+            for p_fixed,p_moving in zip(transformed_point_list, reference_point_list)]
+  if display_errors:
+      from mpl_toolkits.mplot3d import Axes3D
+      import matplotlib.pyplot as plt
+      import matplotlib
+      fig = plt.figure(figsize=figure_size)
+      ax = fig.add_subplot(111, projection='3d')
+      if not min_err:
+          min_err = np.min(errors)
+      if not max_err:
+          max_err = np.max(errors)
+
+      collection = ax.scatter(list(np.array(point_list).T)[0],
+                              list(np.array(point_list).T)[1],
+                              list(np.array(point_list).T)[2],
+                              marker = 'o',
+                              c = errors,
+                              vmin = min_err,
+                              vmax = max_err,
+                              cmap = matplotlib.cm.hot,
+                              label = 'original points')
+      plt.colorbar(collection, shrink=0.8)
+      plt.title('registration errors in mm', x=0.7, y=1.05)
+      ax.set_xlabel('X')
+      ax.set_ylabel('Y')
+      ax.set_zlabel('Z')
+      plt.show()
+
+  return errors
+
 
 
 def print_transformation_differences(tx1, tx2):
